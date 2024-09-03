4e2ac657d4e4bb8a2211f1ed50e6bb609931ca7d
Sergey Chalov <sergeychalov@iMac.local>
2024-09-03 19:06:40 +0300

    update

--- dz_files.py
+++ dz_files.py
@@ -2,15 +2,7 @@ from collections import defaultdict
 import os
 
 def get_cook_book(filename="files/recipes.txt"):
-  """
-  Читает список рецептов из файла и возвращает словарь.
 
-  Args:
-      filename (str): Имя файла с рецептами.
-
-  Returns:
-      dict: Словарь с рецептами, где ключи - названия блюд, а значения - списки словарей с ингредиентами.
-  """
   cook_book = {}
   with open('recipes.txt', "r", encoding="utf-8") as f:
     current_recipe = None
@@ -35,16 +27,7 @@ def get_cook_book(filename="files/recipes.txt"):
   return cook_book
 
 def get_shop_list_by_dishes(dishes, person_count):
-    """
-    Создает список покупок для приготовления блюд.
 
-    Args:
-        dishes (list): Список блюд.
-        person_count (int): Количество персон.
-
-    Returns:
-        dict: Словарь с покупками, где ключи - названия ингредиентов, а значения - словари с количеством и единицей измерения.
-    """
     shop_list = defaultdict(lambda: {"measure": "", "quantity": 0})
     for dish in dishes:
         for ingredient in cook_book[dish]:
@@ -52,30 +35,18 @@ def get_shop_list_by_dishes(dishes, person_count):
             shop_list[ingredient["ingredient_name"]]["quantity"] += (
                 ingredient["quantity"] * person_count
             )
-    return dict(shop_list)  # Преобразуем defaultdict в словарь
+    return dict(shop_list)
 
 def merge_files(folder_path):
-  """
-  Объединяет файлы в папке в один файл по количеству строк,
-  сохраняя служебную информацию о каждом файле.
-
-  Args:
-    folder_path: Путь к папке с файлами.
-
-  Returns:
-    Путь к объединенному файлу.
-  """
-
+ 
   files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
   
-  # Создаем словарь для хранения файлов с количеством строк
   file_data = {}
   for file in files:
     file_path = os.path.join(folder_path, file)
     with open(file_path, 'r') as f:
       file_data[file] = len(f.readlines())
 
-  # Сортируем файлы по количеству строк
   sorted_files = sorted(file_data.items(), key=lambda item: item[1])
 
   # Объединяем файлы
@@ -90,8 +61,7 @@ def merge_files(folder_path):
   return output_file_path
 
 
-# Пример использования
-folder_path = '/Users/sergeychalov/Desktop/Python_DZ/Files/3'  # Замените на путь к вашей папке
+folder_path = '/Users/sergeychalov/Desktop/Python_DZ/Files/3' 
 output_file_path = merge_files('/Users/sergeychalov/Desktop/Python_DZ/Files/3')
 print(f"Объединенный файл сохранен в {output_file_path}")
 
